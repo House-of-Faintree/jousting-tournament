d5b76cb7ba3d20b972d225d9c74769f3e11dcde1
Leo Lou <leo.lou.43@gmail.com>
2015-10-13 16:57:18 +1100

    LCD prototypes

---
+++ Code/Commander/LCD/Working_LCD/lcd.h
@@ -0,0 +1,22 @@
+#ifndef __LCD_H
+#define __LCD_H
+
+#include <p18cxxx.h>
+
+
+
+
+void Lcd_Port(char a);
+void Lcd_Cmd(char a);
+void Lcd_Clear();
+void Lcd_Set_Cursor(char a, char b);
+void Lcd_Init();
+void Lcd_Write_Char(char a);
+void Lcd_Write_String(char *a);
+void Lcd_Shift_Right();
+void Lcd_Shift_Left();
+void delays(int x);
+void delayms(int x);
+void delay10us(int x);
+
+#endif
