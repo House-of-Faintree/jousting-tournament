c07d792e9c7bc98407c1e4028b34a06cdda54b78
deefunkt <deefunktify@gmail.com>
2015-10-15 19:31:46 +1100

    Created new file
    Provides access to adc functions

---
+++ Code/Robot/AD.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   AD.h
+ * Author: Ayush Sharma
+ *
+ * Created on 15 October 2015, 6:03 PM
+ */
+
+#ifndef AD_H
+#define	AD_H
+
+void ADC_setup(void);
+void switchChannels(int channel);
+int doADC(void);
+
+
+#endif	/* AD_H */
+
