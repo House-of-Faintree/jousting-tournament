1d31587b78240f3173a309a5405e034a6c0dee95
deefunkt <deefunktify@gmail.com>
2015-10-19 19:46:15 +1100

    Update MENU_NAV.c
    Refactored code, changed variable names in LCD_disp() function, and tried to add comments to things I understood correctly

--- Code/Commander/LCD/MENU_NAV.c
+++ Code/Commander/LCD/MENU_NAV.c
@@ -7,49 +7,108 @@ These functions were designed to be run on the COMMANDER side, however some may
 be compatible for running on the MOBILE ROBOT side.
 */
 
-//Pseudo code for menu navigation by Leo
+//Code for menu navigation by Leo
+//TODO: 
+//    - try and do naviagtion through function calls
+//    - move interrupt code to interrupts.c
 
-
-#include  <stdio.h>
 #include  <p18cxxx.h>
+#include  <delays.h>
+#include  <string.h>
+#include  "ConfigRegs.h"
+#include  "phrases.h"
+#include  "lcd.h"
+#include  "AD.h"
+
 #define UP 200
 #define DOWN 40
-#define LEFT 200
-#define RIGHT 40
+#define LEFT 40
+#define RIGHT 200
+
+//ATTENTION
+int MNML=0;             //CHANGE DEPENDING ON WHAT BOARD IS USED, 1 FOR MNML
 
 int  RUN;
-int joy_x;
-int joy_y;
-int max_speed=20;
-int pid_gain=50;
-int max_yaw=30;
-int ir_samp_e=60;
-int ir_samp_r=30;
-int ir_raw=30;
-int ir_avg=50;
-int values[7]={max_speed, pid_gain, max_yaw, ir_samp_e, ir_samp_r, ir_raw, ir_avg};
-//Setup
-int menu_ref_1=1;           //Change through ISR
-int menu_ref_2=1;    
+int joy_x=100;
+int joy_y=100;
+int max_speed;
+int pid_gain;
+int max_yaw;
+int ir_samp_e;
+int ir_samp_r;
+int ir_raw;
+int ir_avg;
+
+/*
+ *Pointer Arrays
+ ************************************
+ * These arrays contain pointers to string constants that need to be written
+ * often and do not need to be modified. The strings can be found in
+ * phrases.h
+ */
+char *menutitle[4]={t0,t1,t2,t3}; 
+char *valueStrings[4]={s0,s1,s2,s3};
+
+/****************************************************************************
+ * menu_ref_1 is a reference variable which keeps track of where in the menu
+ * hierarchy the user currently is. It is used as an array index in the array
+ * menutitle[].
+ * **************************************************************************
+ * menu_ref_2 is a reference variable that keeps track of menu subitems, and is
+ * an array index in the array valueStrings[].
+ */
+int menu_ref_1;           //Change through ISR
+int menu_ref_2;  
+int values[8];
+
+
+
+
+//Prototypes
+void welcome(void);
+void delay10us(int x);
+void delayms(int x);
+void delays(int x);
+void LCD_disp(int title_item, int value_item);
 
 
 void main(void){
   
-  RUN==0;
-  
+  max_speed=100;
+  pid_gain=100;
+  max_yaw=100;
+  ir_samp_e=100;
+  ir_samp_r=100;
+  ir_raw=30;
+  ir_avg=50;
+
+  //Default
+  TRISD = 0x00;         //set port D signals to output
+  menu_ref_1=0;
+  menu_ref_2=1;
+
+  Lcd_Init();
+  welcome();
+  ADC_setup();
+  LCD_disp(menu_ref_1, menu_ref_2);
+  RUN=0;
   while(1){
-      
+    switchChannels(0);
+    joy_x = doADC();
+    switchChannels(1);
+    joy_y = doADC();
   
 //NORMAL OPERATION  
     while(RUN==0){
       
-        if(menu_ref_1==1)
+        if(menu_ref_1==0)   //IF IN MANUAL
         {
+            
             if(joy_x<=LEFT){      //User pushes right joystick left
                 if (menu_ref_2<6 && values[menu_ref_2]>0){
                   values[menu_ref_2]-=5;
                 }
-                disp_LCD(menu_ref_1, menu_ref_2);
+                LCD_disp(menu_ref_1, menu_ref_2);
                 delayms(200);
 
             }
@@ -57,7 +116,7 @@ void main(void){
                 if (menu_ref_2<6 && values[menu_ref_2]<100){
                     values[menu_ref_2]+=5;
                 }
-                disp_LCD(menu_ref_1, menu_ref_2);
+                LCD_disp(menu_ref_1, menu_ref_2);
                 delayms(200);
 
             }        
@@ -65,16 +124,16 @@ void main(void){
 
         }  
 
-        if(menu_ref_1==2){            //WHEN IT IS IN FACTORY MODE
+        if(menu_ref_1==1){            //IF IN FACTORY MODE
 
             if (joy_y>=UP){            //User pushes left joystick UP
-              menu_ref_2--
-              //Circular selection
+                menu_ref_2--;
+                //Circular selection
 
                 if(menu_ref_2==0){
                   menu_ref_2=4;
                 }
-                disp_LCD(menu_ref_1, menu_ref_2);
+                LCD_disp(menu_ref_1, menu_ref_2);
                 delayms(250);               //Arbitrary delay of 250 milliseconds
 
             }
@@ -84,19 +143,19 @@ void main(void){
                 if(menu_ref_1==5){
                   menu_ref_2=1;
                 }
-                disp_LCD(menu_ref_1, menu_ref_2);
+                LCD_disp(menu_ref_1, menu_ref_2);
                 delayms(250);
 
             }
             else if (joy_x<=LEFT){      //User pushes right joystick left
-                decrement(menu_ref_2);
-                disp_LCD(menu_ref_1, menu_ref_2);
+                decval(menu_ref_2);
+                LCD_disp(menu_ref_1, menu_ref_2);
                 delayms(100);
 
             }
             else if (joy_x>=RIGHT){     //User pushes right joystick right
-                increment(menu_ref_2);
-                disp_LCD(menu_ref_1, menu_ref_2);
+                incval(menu_ref_2);
+                LCD_disp(menu_ref_1, menu_ref_2);
                 delayms(100);
 
             }            
@@ -107,22 +166,40 @@ void main(void){
   
 }
 
+/* TODO : 
+ * Move code to interrupts.c
+ * 
+ */
   void highPriorityIsr(void)
 {    
+    INTCONbits.GIE = 0;
     //PORT B bit 0 external interrupt for motors on/off button
     if(INTCONbits.INT0IF == 1)
     {
-          if (RUN==0)
-                {
-                RUN==1;
-                Lcd_Clear();
-                Lcd_Set_Cursor(1,1);
-                Lcd_Write_String(t4);     //Motor on message
-                delays(2);
-                Lcd_Clear();
-                Lcd_Set_Cursor(1,1);
-                Lcd_Write_String(t5);
-                }
+     
+        if (RUN==0)
+        {
+            RUN==1;
+            Lcd_Clear();
+            Lcd_Set_Cursor(1,1);
+            Lcd_Write_String(t4);     //Motor on message
+            //MOTOR ON FUNCTION GOES HERE
+            delays(2);
+            Lcd_Clear();
+            Lcd_Set_Cursor(1,1);
+            Lcd_Write_String(t6);     //Display run-time message
+        }
+        else
+        {
+            RUN==0;
+            Lcd_Clear();
+            Lcd_Set_Cursor(1,1);
+            Lcd_Write_String(t5);     //Motor off message
+            //MOTOR OFF FUNCTION GOES HERE
+            delays(2);
+        }
+        
+
     }
 
     //Port B bit 1 external interrupt for menu button
@@ -135,7 +212,116 @@ void main(void){
             }
 
         }
-        INTCON3bits.INT1IF = 0; //clear flag
-        INTCONbits.GIEH = 1; //re-enable interrupts
-        PORTBbits.RB1 = 0; //clear port B bit
-    }        
+    }
+    
+    //Re-enable interrupts
+    INTCON3bits.INT1IF = 0; //clear flag
+    INTCONbits.GIE = 1; //re-enable interrupts
+    PORTBbits.RB1 = 0; //clear port B bit
+}        
+  
+//FUNCTIONS  
+  
+/*This function takes in 2 arguments, corresponding to what should be displayed on
+ * the 1st and 2nd lines of the LCD screen. 
+ * title_item selects which string to display as the title to the menu position.
+ * value_item selects which string to display while changing the submenu item value.
+ */  
+void LCD_disp(int title_item, int value_item)
+{
+    int values[7] = { max_speed, pid_gain, max_yaw, ir_samp_e, ir_samp_r, ir_raw, ir_avg};     
+    char  string[LCDL];
+    Lcd_Clear();
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String(menutitle[title_item]);
+    
+    //Checks if in Manual or Factory mode
+    if (menu_ref_1<=1){ //possibly need to change order of phrases to
+                        //include a check for user assist mode, in which case
+                        //we check if menu_ref_1 <= 2
+        Lcd_Set_Cursor(2,1);    
+        sprintf(string, "%s%d", valueStrings[value_item],values[value_item]);
+        Lcd_Write_String(string);
+        //Rather than checking for which string to send and appending a %,
+        //I added a % to the corresponding string in phrases.h - Ayush
+        
+        /*
+        if (menu_ref_2<=2){
+            Lcd_Set_Cursor(2,1);
+            sprintf(string, "%s%d%", stringtab[y],values[y]);
+            Lcd_Write_String(string);
+        }
+        else {
+            Lcd_Set_Cursor(2,1);    
+            sprintf(string, "%s%d", stringtab[y],values[y]);//no % symbol
+            Lcd_Write_String(string);
+        }
+        */
+    }
+
+    //#############################
+
+}
+
+/*
+ * Simply a function that prints out a standard welcome message on startup.
+ */   
+void welcome(void){
+    Lcd_Clear();
+    Lcd_Set_Cursor(1,1);
+    Lcd_Write_String(w0);
+    Lcd_Set_Cursor(2,1);
+    Lcd_Write_String(w1);
+    delays(5);
+}
+
+
+void delays(int x){
+    if (MNML){
+        for(x;x>0;x--){
+        Delay10KTCYx(100);
+        Delay10KTCYx(100);
+        Delay10KTCYx(50);
+        }
+    }
+    else{
+        for(x;x>0;x--){
+        Delay10KTCYx(100);
+        }
+    }
+}
+
+void delayms(int x){
+    if (MNML){
+        Delay1KTCYx(x);
+        Delay1KTCYx(x);
+        Delay1KTCYx(x);
+
+    }
+    else{
+        Delay1KTCYx(x);
+        
+    }
+}
+
+void delay10us(int x){
+    if (MNML){
+        Delay10TCYx(x);
+        Delay10TCYx(x);
+        Delay10TCYx(x);
+
+    }
+    else{
+        Delay10TCYx(x);      
+    }    
+}
+
+void decval(int x){
+    
+}
+
+void incval(int x){
+    
+}
+
+
