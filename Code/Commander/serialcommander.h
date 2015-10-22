d307b5d8789d56a59819533fed8be24f8aa7d3b0
cccpn7 <cccpn7@gmail.com>
2015-10-22 23:35:48 +1100

    Update serialcommander.h
    some more prototypes

--- Code/Commander/serialcommander.h
+++ Code/Commander/serialcommander.h
@@ -4,6 +4,16 @@
 
 void setupSerial(void);
 int sendByte(char *byte);
+void sendNewLine(void); 
+void sendString(char *byte); 
+void sendNum(int result); 
+void sendHello(void);
+void sendMaxSpeed(void);
+void sendMaxYaw(void);
+void sendIRsampleE(void);
+void sendIRsampleR(void);
+void sendMode(void);
 
+//char *rxptr = GLOBAL_RXBUFFER;
 
 #endif
