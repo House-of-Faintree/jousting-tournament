3aea661d79024741e06f9c74c4e5b7577f77c8c6
cccpn7 <cccpn7@gmail.com>
2015-10-22 23:26:43 +1100

    Update phrases.h
    some shit added

--- Code/Commander/LCD/phrases.h
+++ Code/Commander/LCD/phrases.h
@@ -15,11 +15,11 @@
 //Menu titles
 const char t0[LCDL]="Manual Mode";
 const char t1[LCDL]="Factory Mode";
-const char t2[LCDL]="Full Auto";
-const char t3[LCDL]="Assisted Mode";
+const char t2[LCDL]="Assisted Mode";
+const char t3[LCDL]="Full Auto";
 const char t4[LCDL]="Giddyup!";   //For run time
 const char t5[LCDL]="Whoa!";      //Stop
-const char t6[LCDL]="Running";
+const char t6[LCDL]="Taste my lance!";
 const char w0[LCDL]="Bow, for I am";
 
 #pragma idata DATA_PHRASE
@@ -30,7 +30,7 @@ const char w1[LCDL]="Charlemagne!";
 //NTS exercise care with string lengths, no more than 13 characters
 const char s0[LCDL]="Max speed:";
 const char s1[LCDL]="PID gains:";
-const char s2[LCDL]="Time hor.:"
+const char s2[LCDL]="Time hor.:";
 const char s3[LCDL]="IR samp/est:";
 const char s4[LCDL]="IR samp rate:";
 const char s5[LCDL]="Raw data:";
