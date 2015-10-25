a1471477a745033f835b262e78db8d17a56f7131
Lydia-D <lydia.drabsch@gmail.com>
2015-10-25 22:58:21 +1100

    WORKING ON MINIMAL BOARD
    use this

--- Code/Robot/PWM_motor_turnFull.c
+++ Code/Robot/PWM_motor_turnFull.c
@@ -16,8 +16,8 @@
 #include <stdlib.h>
 //#include <p18f452.h>
 //#include "configReg452.h"
-#include <p18f4520.h>
-#include "configReg4520.h"
+#include <p18cxxx.h>
+#include "Config4520.h"
 
 #define bit(x) (1<<(x)) 
 #define test_bit(var,pos) ((var) & (1<<(pos)))
@@ -33,7 +33,7 @@ unsigned int bit_2A = 4;
 unsigned int bit_2B = 5;
 
 // need to be global to store between files
-unsigned int vel = 0x10; // 'velocity' associated with 50% duty cycle
+unsigned int vel = 0xFF; // 'velocity' associated with 50% duty cycle
 unsigned int omega  = 128;  // 0 turn to start with
 
 //unsigned int max_Dvx=0;
@@ -55,14 +55,35 @@ void direction(unsigned int, unsigned int);
 void turn(void);
 
 
+//void PWM_motor_setup(void){
+//    PR2 = 0xFF;         // set period (FF = 610Hz with timer prescaler 16)
+//    T2CON = 0x07;  // Sets bit 2 to enable timer 2(TMR2), prescaler 16
+//    CCPR2L = 0;   // select duty cycle - start at zero
+//    CCPR1L = 0;   // select duty cycle - start at zero
+//    TRISC =0; // output for CCP1-RC<2> and CCP2-RC<1>
+//    CCP2CON = 0x0C; // Configures CCP2 for PWM mode (CCP2M2 and CCP2M3)
+//    CCP1CON = 0X0C;     
+//}
+
 void PWM_motor_setup(void){
     PR2 = 0xFF;         // set period (FF = 610Hz with timer prescaler 16)
-    T2CON = 0x07;  // Sets bit 2 to enable timer 2(TMR2), prescaler 16
+    //T2CON = 0x07;  // Sets bit 2 to enable timer 2(TMR2), prescaler 16
+    T2CONbits.TMR2ON = 1;
+    T2CONbits.T2OUTPS = 0;
+    //T2CONbits.T2CKPS = 3;
+    T2CONbits.T2CKPS1 = 1;
     CCPR2L = 0;   // select duty cycle - start at zero
     CCPR1L = 0;   // select duty cycle - start at zero
-    TRISC =0; // output for CCP1-RC<2> and CCP2-RC<1>
-    CCP2CON = 0x0C; // Configures CCP2 for PWM mode (CCP2M2 and CCP2M3)
-    CCP1CON = 0X0C;     
+    TRISC = 0; // output for CCP1-RC<2> and CCP2-RC<1>
+    //CCP2CON = 0x0C; // Configures CCP2 for PWM mode (CCP2M2 and CCP2M3)
+    CCP2CONbits.DC2B = 0; // LSBits duty cycle
+   // CCP2CONbits.CCP2M = 15; // PWM mode
+    CCP2CONbits.CCP2M3 = 1;
+    CCP2CONbits.CCP2M2 = 1;
+    CCP1CONbits.DC1B = 0; // duty cycle
+    //CCP1CONbits.CCP1M = 15; // PWM mode
+    CCP1CONbits.CCP1M3 = 1;
+    CCP1CONbits.CCP1M2 = 1;
 }
 
 void Inputs_motor_setup(void){
@@ -71,16 +92,34 @@ void Inputs_motor_setup(void){
     
 }
 
+//void ADC_setup(void){
+//    T0CON = 0xC7; //  11000111  Enables TMR0, 8bit with 256 prescale (p103)
+//    ADCON0 = 0x41;  //0100 0001 Fosc/8, A/D enabled
+//    ADCON1 = 0x0E;  // 0000 1110 Make RA0 analog input, Left justify, 1 analog channel
+//}
+
 void ADC_setup(void){
-    T0CON = 0xC7; //  11000111  Enables TMR0, 8bit with 256 prescale (p103)
-    ADCON0 = 0x41;  //0100 0001 Fosc/8, A/D enabled
-    ADCON1 = 0x0E;  // 0000 1110 Make RA0 analog input, Left justify, 1 analog channel
+    T0CONbits.TMR0ON = 1; //  11000111  Enables TMR0, 8bit with 256 prescale (p103)
+    T0CONbits.T08BIT = 1;
+    T0CONbits.T0CS = 0;
+    T0CONbits.PSA = 0;
+    T0CONbits.T0PS = 7;
+    
+    ADCON0bits.CHS = 0;  //0100 0001 Fosc/8, A/D enabled
+    ADCON0bits.ADON = 1;
+    ADCON1bits.VCFG1 = 0;
+    ADCON1bits.VCFG0 = 0;
+    ADCON1bits.PCFG = 0;
+    ADCON2bits.ADFM = 0;
+    ADCON2bits.ACQT = 0;
+    ADCON2bits.ADCS = 0;
+    TRISA = 0xFF;  // inputs
 }
 
 void direction(unsigned int L_dir, unsigned int R_dir){
     // Left wheel anticlockwise for fwds 01, Right wheel clockwise for fwds 10
-    unsigned int L_inputA = (bit(0)^L_dir) ; // toggle bit
-    unsigned int L_inputB = L_dir;
+    unsigned int L_inputB = (bit(0)^L_dir) ; // toggle bit
+    unsigned int L_inputA = L_dir;
     unsigned int R_inputA = R_dir;
     unsigned int R_inputB = (bit(0)^R_dir) ;// toggle bit
     PORTC = (R_inputA<<bit_1A)|(R_inputB<<bit_1B)|(L_inputA<<bit_2A)|(L_inputB<<bit_2B); // set direction of wheels
@@ -115,11 +154,11 @@ void turn(void){
         R_PWM = 255-(omega-128)*max_Dvx/128;
     } else if(omega<128 && max_Dvx == 5){ // in reverse/forward turning left near full speed
         R_PWM = 255; // run at max
-        L_PWM = 255-(omega-128)*max_Dvx/128;
+        L_PWM = 255-(128-omega)*max_Dvx/128;
     }else if (omega >=128) {            // right turn
         L_PWM = duty - (omega-128)*max_Dvx/128;
         R_PWM = 2*duty-L_PWM;
-    }else{                               // left turn
+    }else if (omega < 128){                               // left turn
         L_PWM = duty + (128-omega)*max_Dvx/128;
         R_PWM = 2*duty-L_PWM;
     }
@@ -139,10 +178,10 @@ void main(void){
 
     
     for(;;){
-    while(INTCONbits.TMR0IF==0){}   // busy loop
-    ADCON0bits.GO=1; 
-    while(PIR1bits.ADIF==0){}   // busy loop wait for conversion to finish
-    vel = ADRESH;
+//    while(INTCONbits.TMR0IF==0){}   // busy loop
+//    ADCON0bits.GO=1; 
+//    while(PIR1bits.ADIF==0){}   // busy loop wait for conversion to finish
+//    vel = ADRESH;
     turn();  // does the 8 bit to unsigned int work?
     
     }
